b2bfcb62e3f416e494eb6d095a7c17b92b6a2970
Adrian Hassa <34100878+Adrian2901@users.noreply.github.com>
2023-04-22 13:48:31 +0200

    Add MQTT subscriber connectivity
    Subscribing to a fixed topic and displaying it #2

--- BoomBoxWebApp/Pages/Index.cshtml.cs
+++ BoomBoxWebApp/Pages/Index.cshtml.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MQTTnet;
+using MQTTnet.Client;
+using System.Text;
 
 namespace BoomBoxWebApp.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    public static string reply = ":)";
 
     public IndexModel(ILogger<IndexModel> logger)
     {
@@ -14,6 +18,55 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+        _ = Handle_Received_Application_Message();
+    }
 
+    public static async Task Handle_Received_Application_Message()
+    {
+        /*
+         * This sample subscribes to a topic and processes the received message.
+         */
+
+        var mqttFactory = new MqttFactory();
+
+        using (var mqttClient = mqttFactory.CreateMqttClient())
+        {
+            var mqttClientOptions = new MqttClientOptionsBuilder().WithWebSocketServer("localhost:9001/mqtt").Build();
+
+            // Setup message handling before connecting so that queued messages
+            // are also handled properly. When there is no event handler attached all
+            // received messages get lost.
+            mqttClient.ApplicationMessageReceivedAsync += e =>
+            {
+                Console.WriteLine("Received application message.");
+
+                reply = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                Console.WriteLine(reply);
+                return Task.CompletedTask;
+
+            };
+
+            await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+            var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
+                .WithTopicFilter(
+                    f =>
+                    {
+                        f.WithTopic("#");
+                    })
+                .Build();
+
+            await mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
+
+            Console.WriteLine("MQTT client subscribed to topic.");
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+        }
+    }
+
+    public JsonResult OnGetReply()
+    {
+        return new JsonResult(reply);
     }
 }
