bcdf738f3b59f18c7986b94d2f05770d4d3e372e
Adrian Hassa <adrianhassa@gmail.com>
2023-04-23 15:50:06 +0200

    Add MQTT publish backend
    Added MQTT publish functionality, taking form inputs as arguments

--- BoomBoxWebApp/Pages/SoundBoard.cshtml.cs
+++ BoomBoxWebApp/Pages/SoundBoard.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MQTTnet;
+using MQTTnet.Client;
 
 namespace BoomBoxWebApp.Pages;
 
@@ -14,6 +16,39 @@ public class SoundBoardModel : PageModel
 
     public void OnGet()
     {
+    }
+
+    //OnPost() will run when submiting a form with method="post"
+    public void OnPost()
+    {
+        //Getting values from the form
+        var brokerIP = Request.Form["Host"];
+        var port = Request.Form["Port"];
+        var topic = Request.Form["Topic"];
+        var payload = Request.Form["Message"];
+
+        //Calling the publishing function
+        _ = Publish_Message(brokerIP, port, topic, payload);
+    }
+
+    public static async Task Publish_Message(string brokerIP, string port, string topic, string payload)
+    {
+        var mqttFactory = new MqttFactory();
+        using (var mqttClient = mqttFactory.CreateMqttClient())
+        {
+            var mqttClientOptions = new MqttClientOptionsBuilder().WithWebSocketServer(brokerIP + ":9001/mqtt").Build();
+
+            await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+            var appMessage = new MqttApplicationMessageBuilder()
+                .WithTopic(topic)
+                .WithPayload(payload)
+                .Build();
+
+            await mqttClient.PublishAsync(appMessage, CancellationToken.None);
+            await mqttClient.DisconnectAsync();
+            Console.WriteLine("MQTT message published");
+        }
     }
 }
 
