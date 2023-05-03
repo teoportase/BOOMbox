78a2cab22008d648c9f61d5e376040fc45aeacbd
ionel <ionelalexandru.pop@gmail.com>
2023-05-03 12:20:41 +0200

    #13 Implment Wio Terminal display functionality The Wio Terminal will now display connection messages to show the user the conection status, and topic messages.

--- WioTerminalConnection/MqttWio/MqttWio.ino
+++ WioTerminalConnection/MqttWio/MqttWio.ino
@@ -2,6 +2,7 @@
 #include "rpcWiFi.h"
 #include <PubSubClient.h>
 #include "ServerData.h"        // file with the server information
+#include "TFT_eSPI.h"
 
 /*
 Some of the following code used for connecting to a WiFi network is taken from the WiFi page
@@ -18,22 +19,22 @@ typedef void (*Callback)(char*, byte*, unsigned int);
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
 
-// To see if the device connects to the WiFi, open the Serial Monitor here.
+// Define lcd screen
+TFT_eSPI tft;
 
-// Gets called when a message is received from the subscribed topic
-void callback(char* topic, byte* message, unsigned int length) {
-  Serial.print("Message received: ");
-  String received_message = "";
-  for (int i = 0; i < length; i++) {
-    received_message += (char)message[i];
-  }
-  Serial.println(received_message);
-}
+// To see if the device connects to the WiFi, open the Serial Monitor.
 
-void setup() {  
+void setup() {
+
+  //LCD Setup
+  tft.begin();
+  tft.setRotation(3);
+  tft.fillScreen(TFT_RED);
+  tft.setTextColor(TFT_BLACK);
+  tft.setTextSize(4);
+
   // Starts the first connection
   Serial.begin(115200);
-  while(!Serial); // Wait for Serial to be ready
 
   Serial.println("Connecting to WiFi..");
   WiFi.begin(SSID, PASSWORD);
@@ -45,18 +46,26 @@ void setup() {
   }
   Serial.println("Connected to the WiFi network");
 
+  printOnScreen("Connected to the WiFi network", 10, 100, 3);
+
   client.setServer(MQTT_SERVER, MQTT_PORT);
   client.setCallback(callback);
 
   // Connect to MQTT broker
   while (!client.connected()) {
     Serial.print("Connecting to MQTT broker...");
+    
+    tft.fillScreen(TFT_RED);
+    tft.drawString("Connecting to broker...", 10, 100);
     if (client.connect("Wio Terminal")) {
       Serial.println("connected");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());   // to see more about the reasons the connection failed, check: https://pubsubclient.knolleary.net/api#state
       Serial.println(" retrying in 5 seconds");
+      
+      printOnScreen("Failed to connect to broker :(", 10, 50, 3);
+      printOnScreen("Retrying in 5 seconds...", 10, 100, 3);
       delay(5000);
     }
   }
@@ -64,7 +73,11 @@ void setup() {
   // The device subscribes to the topic
   client.subscribe(MQTT_TOPIC);
 
+  // Publish test message
   client.publish("test", "help me lord");
+
+  // Display successful connection
+  printOnScreen("Connected to broker!", 10, 100, 3);
 }
 
 void loop() {
@@ -83,4 +96,24 @@ void loop() {
       delay(5000);
     }
   }
+
+  client.loop();
+}
+
+// Gets called when a message is received from the subscribed topic
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message received: ");
+  String received_message = "";
+  for (int i = 0; i < length; i++) {
+    received_message += (char)message[i];
+  }
+  Serial.println(received_message);
+
+  printOnScreen(received_message, 10, 100, 4);
+}
+
+void printOnScreen(String message, int x, int y, int size) {
+  tft.fillScreen(TFT_RED);
+  tft.drawString(message, x, y);
+  tft.setTextSize(4);
 }
