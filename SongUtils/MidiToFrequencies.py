0f5cb45f08c9e8391264de997320c08973676bfd
ionel <ionelalexandru.pop@gmail.com>
2023-05-11 17:49:16 +0200

    Modify note duration Co-authored-by: @Adrian Hassa <gushasade@student.gu.se>

--- SongUtils/MidiToFrequencies.py
+++ SongUtils/MidiToFrequencies.py
@@ -167,7 +167,7 @@ def check_float(duration, quarter_note_duration=100):
     return round(durationInMilliseconds)
 
 def getQuarterNoteDuration(bpm):
-    return round((float(bpm) / 60) * 100)
+    return round((float(bpm) / 60) * 200)
 
 file = input('Enter the path of the file: ')
 mainInstrument = input('Enter the main instrument you would like to parse: (d: Piano) ')
